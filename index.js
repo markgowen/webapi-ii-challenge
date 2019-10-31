fcae8d0bedd963ba8c449e00817db163ec00f5f5
Mark Gowen <mark.gowen78@gmail.com>
2019-10-31 10:51:15 -0700

    commit for update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const postRouter = require('./data/routing/post-router');
@@ -7,8 +8,9 @@ const server = express();
 server.use(express.json());
 server.use('/api/posts', postRouter);
 
+console.log(`message:`, process.env.MSG);
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log('Server Running on http://localhost:5000');
+  console.log(`Server Running on ${port}`);
 });
